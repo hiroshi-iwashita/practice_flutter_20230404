757e1b9811dbb2ea72b83825301e16a26a2aae3d
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2023-04-10 16:11:06 +0900

    add: ListTile

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -34,47 +34,53 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               // Column with SingleChildScrollView, this needs to have fixed height.
               itemBuilder: (ctx, index) {
-                return Card(
-                  child: Row(children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${transactions[index].amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transactions[index].title,
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                        Text(
-                          DateFormat('yyyy-MM-dd')
-                              .format(transactions[index].date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ]),
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Text('\$${transactions[index].amount}'),
+                  ),
                 );
+                // return Card(
+                //   child: Row(children: <Widget>[
+                //     Container(
+                //       margin: EdgeInsets.symmetric(
+                //         vertical: 10,
+                //         horizontal: 15,
+                //       ),
+                //       decoration: BoxDecoration(
+                //         border: Border.all(
+                //           color: Theme.of(context).primaryColor,
+                //           width: 2,
+                //         ),
+                //       ),
+                //       padding: EdgeInsets.all(10),
+                //       child: Text(
+                //         '\$${transactions[index].amount.toStringAsFixed(2)}',
+                //         style: TextStyle(
+                //           fontWeight: FontWeight.bold,
+                //           fontSize: 20,
+                //           color: Theme.of(context).primaryColor,
+                //         ),
+                //       ),
+                //     ),
+                //     Column(
+                //       crossAxisAlignment: CrossAxisAlignment.start,
+                //       children: <Widget>[
+                //         Text(
+                //           transactions[index].title,
+                //           style: Theme.of(context).textTheme.title,
+                //         ),
+                //         Text(
+                //           DateFormat('yyyy-MM-dd')
+                //               .format(transactions[index].date),
+                //           style: TextStyle(
+                //             color: Colors.grey,
+                //           ),
+                //         ),
+                //       ],
+                //     ),
+                //   ]),
+                // );
               },
               itemCount: transactions.length,
             ),
