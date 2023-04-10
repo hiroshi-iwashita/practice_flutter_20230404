0723ffe4e5e0f6598da511227fcf54075a89d980
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2023-04-10 17:15:58 +0900

    add: delete transaction

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.purple,
           accentColor: Colors.amber,
+          errorColor: Colors.red,
           fontFamily: 'Quicksand',
           textTheme: ThemeData.light().textTheme.copyWith(
                 title: TextStyle(
@@ -69,18 +70,6 @@ class _MyHomePageState extends State<MyHomePage> {
       date: DateTime.now(),
     ),
     Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.66,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.66,
-      date: DateTime.now(),
-    ),
-    Transaction(
       id: 't2',
       title: 'Weekly Groceries',
       amount: 16452.66,
@@ -98,11 +87,11 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  void _addNewTransaction(String txTitle, double txAmount) {
+  void _addNewTransaction(String txTitle, double txAmount, DateTime chosenDate) {
     final newTx = Transaction(
       title: txTitle,
       amount: txAmount,
-      date: DateTime.now(),
+      date: chosenDate,
       id: DateTime.now().toString(),
     );
 
@@ -124,6 +113,16 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       },
     );
+  }
+
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransactions.removeWhere((tx) => tx.id == id);
+      // same as below
+      // _userTransactions.removeWhere((tx) {
+      //   return tx.id == id;
+      // });
+    });
   }
 
   @override
@@ -149,7 +148,7 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Chart(_recentTransactions),
-            TransactionList(_userTransactions),
+            TransactionList(_userTransactions, _deleteTransaction),
           ],
         ),
       ),
